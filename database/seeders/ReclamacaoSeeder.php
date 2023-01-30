0c707ace076d0cbb1004899937981220393aeee9
mareloo <81195877+mareloo@users.noreply.github.com>
2023-01-30 11:04:58 +0200

    Update ReclamacaoSeeder.php

--- database/seeders/ReclamacaoSeeder.php
+++ database/seeders/ReclamacaoSeeder.php
@@ -20,28 +20,28 @@ class ReclamacaoSeeder extends Seeder
         	[
 	        	'caso' => 'rua desagradada',
 	        	'mensagem' => ' mensagem Descrição da reclamação ',
-	        	'referencia' => 60420,
+	        
 	        	'contacto' => '841801900',
 				'rua' => 'av.bagamoyo',
 				'bairro' => 'bairro',
 				'file' => '841801900',
 				'senha' => '841801900',
 				'posto' => '',
-				'id_user'=> 2,
+				'id_user'=> 4,
 				'created_at' => date('Y-m-d H:i:s'),
 				'updated_at' => date('Y-m-d H:i:s')
         	],
         	[
 	        	'caso' => 'rua desagradada',
 	        	'mensagem' => ' mensagem Descrição da reclamação ',
-	        	'referencia' => 750452,
+	     
 	        	'contacto' => '841801900',
 				'rua' => 'av.bagamoyo',
 				'bairro' => 'bairro',
 				'file' => '841801900',
 				'senha' => '841801900',
 				'posto' => '',
-				'id_user'=> 1,
+				'id_user'=> 4,
 				'created_at' => date('Y-m-d H:i:s'),
 				'updated_at' => date('Y-m-d H:i:s')
         	]
